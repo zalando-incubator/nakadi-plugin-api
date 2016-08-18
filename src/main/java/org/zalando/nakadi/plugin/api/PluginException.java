24b68fb00ee431564ee36368079468100a7f0f50
Viktor Buldakov <viktor.buldakov@zalando.de>
2016-08-18 16:14:06 +0200

    add super methods to exception

--- src/main/java/org/zalando/nakadi/plugin/api/PluginException.java
+++ src/main/java/org/zalando/nakadi/plugin/api/PluginException.java
@@ -1,4 +1,16 @@
 package org.zalando.nakadi.plugin.api;
 
 public class PluginException extends RuntimeException {
+
+    public PluginException(String message) {
+        super(message);
+    }
+
+    public PluginException(String message, Throwable e) {
+        super(message, e);
+    }
+
+    public PluginException(Throwable e) {
+        super(e);
+    }
 }

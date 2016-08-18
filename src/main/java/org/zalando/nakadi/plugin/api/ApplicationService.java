db2fee4c0daa80586e48aa648803ef1e2512ee38
Viktor Buldakov <viktor.buldakov@zalando.de>
2016-08-18 15:08:43 +0200

    ARUHA-123: fix problems

--- src/main/java/org/zalando/nakadi/plugin/api/ApplicationService.java
+++ src/main/java/org/zalando/nakadi/plugin/api/ApplicationService.java
@@ -3,6 +3,8 @@ package org.zalando.nakadi.plugin.api;
 public interface ApplicationService {
 
     /**
+     * @param applicationId Application Id to validate
+     * @return true if application exists false if isn't
      * @throws PluginException if an error occurred while execute
      */
     boolean exists(String applicationId);
--- src/main/java/org/zalando/nakadi/plugin/api/ApplicationServiceFactory.java
+++ src/main/java/org/zalando/nakadi/plugin/api/ApplicationServiceFactory.java
@@ -3,6 +3,8 @@ package org.zalando.nakadi.plugin.api;
 public interface ApplicationServiceFactory {
 
     /**
+     * @param properties system properties to initialize plugin
+     * @return constructed ApplicationService instance
      * @throws PluginException if an error occurred on plugin initialization
     */
     ApplicationService init(SystemProperties properties);

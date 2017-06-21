c0fc17d59fd510d7e395410084aed7b5913704c3
Lionel Montrieux <lionel.montrieux@zalando.de>
2017-06-21 13:00:59 +0200

    ARUHA-759 Rename 'category' to 'dataType'
    This conforms to the XACML terminology

--- src/main/java/org/zalando/nakadi/plugin/api/authz/AuthorizationAttribute.java
+++ src/main/java/org/zalando/nakadi/plugin/api/authz/AuthorizationAttribute.java
@@ -2,16 +2,16 @@ package org.zalando.nakadi.plugin.api.authz;
 
 public class AuthorizationAttribute {
 
-    private final String category;
+    private final String dataType;
     private final String value;
 
-    public AuthorizationAttribute(final String category, final String value) {
-        this.category = category;
+    public AuthorizationAttribute(final String dataType, final String value) {
+        this.dataType = dataType;
         this.value = value;
     }
 
-    public String getCategory() {
-        return category;
+    public String getDataType() {
+        return dataType;
     }
 
     public String getValue() {
